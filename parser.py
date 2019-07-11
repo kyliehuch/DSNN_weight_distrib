40db354c70e3f80803cb6422f83a5597f9123ba6
kyliehuch <kyliehuch@umail.ucsb.edu>
2019-07-11 13:42:57 -0700

    Modified parser.py

--- parser.py
+++ parser.py
@@ -8,11 +8,27 @@ for param_tensor in state_dict:
         #print(param_tensor, "\t", state_dict[param_tensor])
         #for neuron in state_dict[param_tensor][0]:
 
+conv1_weights = []
+for k in range(32):
+    cur_weights = []
+    for i in range(2):
+        for j in range(5):
+            for weight in state_dict["conv1.weight"][k,i,j,:]:
+                cur_weights.append(weight.item())
+    conv1_weights.append(cur_weights)
+
+
+print("number weights:", len(conv1_weights[0]))
+print(conv1_weights[0])
+
+
+'''
 c1_n0_weights = []
 for i in range(2):
     for j in range(5):
         for weight in state_dict["conv1.weight"][0,i,j,:]:
-                c1_n0_weights.append(weight.item())
+            c1_n0_weights.append(weight.item())
 
 print("number weights:", len(c1_n0_weights))
 print(c1_n0_weights)
+'''
