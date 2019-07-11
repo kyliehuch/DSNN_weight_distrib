b440e21de8eaabbe5f7be0654ad3ad48ba5beb94
kyliehuch <kyliehuch@umail.ucsb.edu>
2019-07-11 15:55:35 -0700

    Added extractor.py to extract weights to separate file

--- extractor.py
+++ extractor.py
@@ -1,34 +1,29 @@
 import torch
 import os
 
+f = open("kherweights.txt","w")
+
 state_dict = torch.load("saved_l2.net")
 for param_tensor in state_dict:
     if "stdp" not in param_tensor:
-        print(param_tensor, "\t", state_dict[param_tensor].size())
+        f.write(param_tensor, "\t", state_dict[param_tensor].size(), "\n")
 
-'''
-conv1_weights = []
+f.write("conv1 weights:\n")
 for k in range(32):
     cur_weights = []
     for i in range(2):
         for j in range(5):
             for weight in state_dict["conv1.weight"][k,i,j,:]:
                 cur_weights.append(weight.item())
-    conv1_weights.append(cur_weights)
+    f.write("c1n", k, "_wts = ", cur_weights, "\n")
 
-conv2_weights = []
+f.write("\nconv1 weights:\n")
 for k in range(150):
     cur_weights = []
     for i in range(32):
         for j in range(2):
             for weight in state_dict["conv1.weight"][k,i,j,:]:
                 cur_weights.append(weight.item())
-    conv2_weights.append(cur_weights)
+    f.write("c2n", k, "_wts = ", cur_weights, "\n")
 
-
-print("number weights, c1[0]:", len(conv1_weights[0]))
-print(conv1_weights[0])
-
-print("number weights, c1[1]:", len(conv1_weights[1]))
-print(conv1_weights[1])
-'''
+f.close()
