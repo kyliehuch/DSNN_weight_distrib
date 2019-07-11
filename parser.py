1c93a277904fa40260960b29457f19346850e8af
kyliehuch <kyliehuch@umail.ucsb.edu>
2019-07-11 15:01:16 -0700

    Added support for both layers

--- parser.py
+++ parser.py
@@ -8,6 +8,7 @@ for param_tensor in state_dict:
         #print(param_tensor, "\t", state_dict[param_tensor])
         #for neuron in state_dict[param_tensor][0]:
 
+'''
 conv1_weights = []
 for k in range(32):
     cur_weights = []
@@ -17,18 +18,19 @@ for k in range(32):
                 cur_weights.append(weight.item())
     conv1_weights.append(cur_weights)
 
+conv2_weights = []
+for k in range(150):
+    cur_weights = []
+    for i in range(32):
+        for j in range(2):
+            for weight in state_dict["conv1.weight"][k,i,j,:]:
+                cur_weights.append(weight.item())
+    conv2_weights.append(cur_weights)
 
-print("number weights:", len(conv1_weights[0]))
+
+print("number weights c1[0]:", len(conv1_weights[0]))
 print(conv1_weights[0])
 
-
-'''
-c1_n0_weights = []
-for i in range(2):
-    for j in range(5):
-        for weight in state_dict["conv1.weight"][0,i,j,:]:
-            c1_n0_weights.append(weight.item())
-
-print("number weights:", len(c1_n0_weights))
-print(c1_n0_weights)
+print("number weights c1[1]:", len(conv1_weights[1]))
+print(conv1_weights[1])
 '''
