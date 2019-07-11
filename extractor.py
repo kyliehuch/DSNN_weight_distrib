cbcba31742d488c165eac7fdf60aafa61b4acc94
Kylie <kylie.huch@gmail.com>
2019-07-11 16:09:07 -0700

    Modified extractor.py

--- extractor.py
+++ extractor.py
@@ -6,7 +6,8 @@ f = open("kherweights.txt","w")
 state_dict = torch.load("saved_l2.net")
 for param_tensor in state_dict:
     if "stdp" not in param_tensor:
-        f.write(param_tensor, "\t", state_dict[param_tensor].size(), "\n")
+        #f.write(param_tensor + "\t" + state_dict[param_tensor].size() + "\n")
+        print(param_tensor, "\t", state_dict[param_tensor].size(), "\n")
 
 f.write("conv1 weights:\n")
 for k in range(32):
@@ -15,7 +16,7 @@ for k in range(32):
         for j in range(5):
             for weight in state_dict["conv1.weight"][k,i,j,:]:
                 cur_weights.append(weight.item())
-    f.write("c1n", k, "_wts = ", cur_weights, "\n")
+    f.write("c1n" + k + "_wts = " + cur_weights + "\n")
 
 f.write("\nconv1 weights:\n")
 for k in range(150):
@@ -24,6 +25,6 @@ for k in range(150):
         for j in range(2):
             for weight in state_dict["conv1.weight"][k,i,j,:]:
                 cur_weights.append(weight.item())
-    f.write("c2n", k, "_wts = ", cur_weights, "\n")
+    f.write("c2n" + k + "_wts = " + cur_weights + "\n")
 
 f.close()
