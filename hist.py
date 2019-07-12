8649c261ed2c2b7d6b3f2ee5a3019c2518b121e6
kyliehuch <kyliehuch@umail.ucsb.edu>
2019-07-11 19:26:01 -0700

    added sparsity readout

--- hist.py
+++ hist.py
@@ -19,18 +19,24 @@ while (flag == True):
             for j in range(5):
                 for wt in state_dict["conv1.weight"][NRN_INDX,i,j,:]:
                     wts.append(wt.item())
+        num_inps = 50
+        thr = 10
         flag = False
     elif (LAYER == 2):
         for i in range(32):
             for j in range(2):
                 for wt in state_dict["conv2.weight"][NRN_INDX,i,j,:]:
                     wts.append(wt.item())
+        num_inps = 128
+        thr = 1
         flag = False
     else:
         print("Please enter layer index of 1 or 2")
 
 
-plt.hist(wts, bins=20, range=(0.00, 1.00))
+(n, bins, patches) = plt.hist(wts, bins=20, range=(0.00, 1.00))
+print("threshold/num_inputs = {}".format(thr/num_inps))
+print("number of weights close to 1 = {}".format(n[19]))
 plt.title("Histogram of c{}n{} neuron's weight distribution".format(LAYER, NRN_INDX))
 plt.xlabel('weight')
 plt.ylabel('number weights')
